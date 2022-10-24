b6203bb00ccaa15d20eac9ed48983cd461389b06
katerina-sys <13245545Zx>
2022-10-24 20:39:55 +0300

    Initial commit

---
+++ ads/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Ad(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    author = models.CharField(max_length=50)
+    price = models.PositiveIntegerField()
+    description = models.CharField(max_length=300)
+    address = models.CharField(max_length=200)
+    is_published = models.BooleanField(default=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
